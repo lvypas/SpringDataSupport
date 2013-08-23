c447ba305718a97b7ec74a56a81785d1c80d3326
lvypas <lyubic@gmail.com>
2013-08-23 15:03:56 +0300

    Tutorial #39 - Using JdbcTemplete

--- src/org/lvypas/JdbcDemo.java
+++ src/org/lvypas/JdbcDemo.java
@@ -1,18 +1,18 @@
 package org.lvypas;
 
 import org.lvypas.dao.JdbcDaoImpl;
-import org.lvypas.model.Circle;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class JdbcDemo {
-    
-    public static void main(String[] args){
-        
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(
+                "spring.xml");
         JdbcDaoImpl dao = ctx.getBean("jdbcDaoImpl", JdbcDaoImpl.class);
-        Circle circle = dao.getCircle(1);
-        System.out.println(circle.getName());
+        System.out.println(dao.getCircleCount());
+
     }
 
 }
--- src/org/lvypas/dao/JdbcDaoImpl.java
+++ src/org/lvypas/dao/JdbcDaoImpl.java
@@ -9,49 +9,66 @@ import javax.sql.DataSource;
 
 import org.lvypas.model.Circle;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JdbcDaoImpl {
-    @Autowired
+
     private DataSource dataSource;
-    
+    private JdbcTemplate jdbcTemplete = new JdbcTemplate();
+
     public DataSource getDataSource() {
         return dataSource;
     }
 
+    @Autowired
     public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
+        this.jdbcTemplete = new JdbcTemplate(dataSource);
     }
 
-    public Circle getCircle(int circleId){
-        
+    public JdbcTemplate getJdbcTemplete() {
+        return jdbcTemplete;
+    }
+
+    public void setJdbcTemplete(JdbcTemplate jdbcTemplete) {
+        this.jdbcTemplete = jdbcTemplete;
+    }
+
+    public Circle getCircle(int circleId) {
+
         Connection conn = null;
         Circle circle = null;
-        
+
         try {
-            
+
             conn = dataSource.getConnection();
-            PreparedStatement ps = conn.prepareStatement("SELECT * FROM circle where id = ?");
-            ps.setInt(1, circleId);            
-            
+            PreparedStatement ps = conn
+                    .prepareStatement("SELECT * FROM circle where id = ?");
+            ps.setInt(1, circleId);
+
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
                 circle = new Circle(circleId, rs.getString("name1"));
             }
-            
+
             rs.close();
             ps.close();
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             throw new RuntimeException(ex);
         } finally {
             try {
                 conn.close();
             } catch (SQLException e) {
-                
-            }            
-        }        
+
+            }
+        }
         return circle;
+    }
+
+    public int getCircleCount() {
+        String sql = "SELECT COUNT(*) FROM CIRCLE";
+        return jdbcTemplete.queryForInt(sql);
     }
 
 }
